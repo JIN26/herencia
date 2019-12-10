2591ac3a12c57a1fa070f7048f72ddca371c9401
Angel Mantilla <32878957+JIN26@users.noreply.github.com>
2019-12-10 17:12:17 -0500

    Update Estudiante
    implementación de Ipersona

--- src/clases/Estudiante.java
+++ src/clases/Estudiante.java
@@ -7,12 +7,13 @@ package clases;
  * @author CYBER-SERVER
  *
  */
-public class Estudiante extends Persona{
+public class Estudiante extends Persona implements Ipersona {
   //atrib
   private int const Codigo;
   
   //constructores 
   public Estudiante(){
+    super();
     this.codigo = 0;
   }
   public Estuante(int Codigo){
