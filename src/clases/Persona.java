a6e1a052d1af756aec4c15ce787dcbe9ef3faa56
Angel Mantilla <32878957+JIN26@users.noreply.github.com>
2019-12-10 16:39:00 -0500

    update Persona.java
    clase abstracta terminada

--- src/clases/Persona.java
+++ src/clases/Persona.java
@@ -8,5 +8,22 @@ package clases;
  *
  */
 public abstract class Persona {
-
+  //atrib 
+  String nombre;
+  String apellido;
+  int cedula;
+  //contrucctores
+  Direccion direccion;
+  public Persona(){
+    this.nombre=" ";
+    this.apellido=" ";
+    this.cedula=0;
+    this.direccion = new Direccion();
+  }
+  public Persona(String nombre,String apellido,int cedula,Direccion direccion){
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.cedula = cedula;
+    this.direccion = direccion;
+  }
 }
