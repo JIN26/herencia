e83fe44f9fbf9e70b45237cee6473c81a03f9b83
Angel Mantilla <32878957+JIN26@users.noreply.github.com>
2019-12-10 16:47:28 -0500

    Create Profesor.java
    se creo una clase profesor y se le aplico herencia

---
+++ src/clases/Profesor.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package clases;
+
+/**
+ * @author CYBER-SERVER
+ *
+ */
+public class Profesor extends Persona{
+  //atrib
+  String tipo;
+  
+  //constructores
+  public Profesor(){
+    this.tipo = " ";
+  }
+  public Profesor(String tipo){
+    this.tipo =tipo
+  }
+}
